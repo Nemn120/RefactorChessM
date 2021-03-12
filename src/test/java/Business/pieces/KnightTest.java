8875f93d93f62b1ab013a8b55c423d2c61c155f6
Osti Katriel <katriel181920@example.com>
2021-03-12 11:49:07 -0500

    test Knight complete

--- src/test/java/Business/pieces/KnightTest.java
+++ src/test/java/Business/pieces/KnightTest.java
@@ -1,31 +1,36 @@
 package Business.pieces;
 
 import GUI.board.ChessGameBoard;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class KnightTest {
 
     Knight caballoBlack;
-    Knight caballoWhite;
+    ChessGameBoard chessGameBoard;
 
     @Before
     public void before(){
-        caballoBlack=new Knight(new ChessGameBoard(),0,1,0);//row=0;col=1,6;color=0 black
-        caballoWhite=new Knight(new ChessGameBoard(),1,1,1);//row=1,2,3...;col=1,6;color=1 white
+        chessGameBoard=new ChessGameBoard();
+        caballoBlack=new Knight(chessGameBoard,0,1,0);//row=0; col=1,6; color=0 black
     }
+
 
     @Test
     public void testCalculatePossibleMovesBlack(){
-        System.out.println("caballoBlack: "+caballoBlack.calculatePossibleMoves(new ChessGameBoard()));
-        assertNotNull(caballoBlack.calculatePossibleMoves(new ChessGameBoard()));
+        ArrayList<String> esperado=new ArrayList<>();
+        esperado.add("2,2");
+        esperado.add("2,0");
+        ArrayList<String> actual=caballoBlack.calculatePossibleMoves(chessGameBoard);
+        System.out.println("esperadoCaballoBlack: "+esperado);
+        System.out.println("actualCaballoBlack: "+actual);
+
+        Assert.assertArrayEquals(esperado.toArray(),actual.toArray());
     }
 
-    @Test
-    public void testCalculatePossibleMovesWhite(){
-        System.out.println("caballoWhite: "+caballoWhite.calculatePossibleMoves(new ChessGameBoard()));
-        assertNotNull(caballoWhite.calculatePossibleMoves(new ChessGameBoard()));
-    }
 }
